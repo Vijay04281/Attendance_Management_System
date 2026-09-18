3fe173d29560f7003fb14ee34d88c6dad128f42d
Vijayp4281 <71382506058.vijay@sritcbe.ac.in>
2026-09-18 17:18:34 +0530

    Fix CORS34

--- backend/src/server.js
+++ backend/src/server.js
@@ -166,10 +166,6 @@ const corsOptions = {
 app.use(
     cors(corsOptions)
 );
-app.options(
-    "*",
-    cors(corsOptions)
-);
 // =====================================================
 // OPTIONS / PREFLIGHT
 // =====================================================
